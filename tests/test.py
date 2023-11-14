bf4a4532c9848f66733d35706173194f974e85ec
Elias Weston-Farber <elias@Eliass-MacBook-Pro-2.local>
2023-11-14 03:02:49 -0500

    improving tests

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,17 @@
 import multiprocessing
 import unittest
+from src.jarvis_conversationalist.logger_config import get_logger
 from src.jarvis_conversationalist import process_assistant_response, get_core_path
 from src.jarvis_conversationalist.audio_player import play_audio_file
 
 
 class TestJarvisConversationalist(unittest.TestCase):
+
+    def test_get_logger(self):
+        # Define a test case
+        logger = get_logger()
+        # Assert that the function returns the expected result
+        self.assertIsNotNone(logger)
 
     def test_play_audio_file(self):
         # Define a test case
