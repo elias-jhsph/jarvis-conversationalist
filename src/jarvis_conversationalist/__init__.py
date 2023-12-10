f6e692eac75235c123591b3b439f42aa6a02050b
Elias <56934916+elias-jhsph@users.noreply.github.com>
2023-12-10 17:13:22 -0500

    Update __init__.py

--- src/jarvis_conversationalist/__init__.py
+++ src/jarvis_conversationalist/__init__.py
@@ -2,5 +2,5 @@ import sys
 if sys.platform == 'linux':
     __import__('pysqlite3')
     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-__version__ = '0.3.2'
+__version__ = '0.4.3'
 # Path: src/jarvis_conversationalist/conversationalist.py
