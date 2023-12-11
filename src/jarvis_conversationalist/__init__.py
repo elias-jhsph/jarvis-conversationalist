291f9ee8546ce4a24770b364bdb7d00b0c5cfaa6
Elias Weston-Farber <elias@Eliass-MacBook-Pro-2.local>
2023-12-11 03:42:59 -0500

    temporary override of direction and completion checks while finetuning conversational management model

--- src/jarvis_conversationalist/__init__.py
+++ src/jarvis_conversationalist/__init__.py
@@ -2,5 +2,5 @@ import sys
 if sys.platform == 'linux':
     __import__('pysqlite3')
     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 # Path: src/jarvis_conversationalist/conversationalist.py
